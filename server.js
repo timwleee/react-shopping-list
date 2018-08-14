9d4196f968e29d5b7ae942a92c8433da79e8ee84
Tim Lee <leewtim@gmail.com>
2018-08-14 14:04:57 -0700

    Deployed to Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+
 const items = require('./server/routes/api/items');
 
 
@@ -18,6 +20,16 @@ mongoose
 
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const port = process.env.PORT || 5000;
 
